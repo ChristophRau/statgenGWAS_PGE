97f3e05c6065a2ee5b61db5aa9fbf7abb4564ec4
Rossum, Bart-Jan van <bart-jan.vanrossum@wur.nl>
2020-11-04 10:27:31 +0100

    Commented function

--- R/annotateSNP.R
+++ R/annotateSNP.R
@@ -6,23 +6,29 @@
 annotateSNP <- function(GWAS,
                         gff) {
   signSnp <- do.call(rbind, GWAS$signSnp)
-  
+  ## Read gff (https://en.wikipedia.org/wiki/General_feature_format).
   gffDat <- data.table::fread(gff, skip = 1, na.strings = c("###", "."), 
                               fill = TRUE, sep = "\t")
-  colnames(gffDat) <- c("seqid", "source", "type", "start", "end", "score",
-                      "strand", "phase", "attributes")
-  gffDat <- gffDat[!is.na(gffDat[["seqid"]]), ]
-  gffDat <- gffDat[gffDat[["type"]] == "gene"]
-  
+  ## Set colnames as described on wiki.
+  colnames(gffDat) <- c("sequence", "source", "feature", "start", "end", "score",
+                        "strand", "phase", "attributes")
+  ## Remove empty sequences and select genes.
+  gffDat <- gffDat[!is.na(gffDat[["sequence"]]), ]
+  gffDat <- gffDat[gffDat[["feature"]] == "gene"]
+  ## Replace chromosomes by their numeric value.
+  ## This might give warnings for non-numeric chromosomes.
+  ## These are converted to NA and have to be removed.
   gffDat[["chr"]] <- suppressWarnings(as.numeric(gsub(pattern = "Chr", 
                                                       replacement = "",
-                                                      x = gffDat[["seqid"]])))
+                                                      x = gffDat[["sequence"]])))
   gffDat <- gffDat[!is.na(gffDat[["chr"]]), ]
-  
+  ## Not allocating memory causes an error in the merge step.
+  ## This is a known problem in data.table.
   data.table::setalloccol(gffDat, 2048)
   annoDat <- gffDat[signSnp, on = c("chr", "start <= pos", "end >= pos"), 
                     nomatch = NA]
-  
+  ## Process attributes to extract gene IDs.
+  ## Attributes are assumed to be a ; separated list with items of the form a=b. 
   subAttribs <- strsplit(annoDat[["attributes"]], split = ";")
   annoDat[["ID"]] <- sapply(X = subAttribs, FUN = function(subAttrib) {
     if (!is.na(subAttrib[1])) {
@@ -33,9 +39,7 @@ annotateSNP <- function(GWAS,
       NA
     }
   })
-  
+  ## Convert to list with genes per SNP.
   annoDat2 <- split(annoDat[["ID"]], annoDat[["snp"]])
-  
   return(annoDat2)
 }
-
